d72fe44225cb00af5692459685deb6d2b1b81b07
JasperDemulder <jasperdemulderdev@gmail.com>
2015-11-06 21:25:36 +0100

    fix for mirrored y-axis on guns

--- Assets/Scripts/PlayerGuns.cs
+++ Assets/Scripts/PlayerGuns.cs
@@ -28,10 +28,10 @@ public class PlayerGuns : MonoBehaviour
 		Transform oldRight = RTurret;
 
 		if ((XCI.GetAxis (XboxAxis.RightStickX) < - precision) || (XCI.GetAxis (XboxAxis.RightStickX) > precision) || (XCI.GetAxis (XboxAxis.RightStickY) < - precision) || (XCI.GetAxis (XboxAxis.RightStickY) > precision)) {
-			LTurret.localRotation = Quaternion.Lerp (oldLeft.localRotation, Quaternion.Euler (0, Mathf.Atan2 (XCI.GetAxis (XboxAxis.RightStickX), -XCI.GetAxis (XboxAxis.RightStickY)) * 180 / Mathf.PI, 0), .5f);
+			LTurret.localRotation = Quaternion.Lerp (oldLeft.localRotation, Quaternion.Euler (0, Mathf.Atan2 (XCI.GetAxis (XboxAxis.RightStickX), XCI.GetAxis (XboxAxis.RightStickY)) * 180 / Mathf.PI, 0), .5f);
 		}
 		if ((XCI.GetAxis (XboxAxis.LeftStickX) < - precision) || (XCI.GetAxis (XboxAxis.LeftStickY) > precision) || (XCI.GetAxis (XboxAxis.LeftStickY) < - precision) || (XCI.GetAxis (XboxAxis.LeftStickY) > precision)) {
-			RTurret.localRotation = Quaternion.Lerp (oldRight.localRotation, Quaternion.Euler (0, Mathf.Atan2 (XCI.GetAxis (XboxAxis.LeftStickX), -XCI.GetAxis (XboxAxis.LeftStickY)) * 180 / Mathf.PI, 0), .5f);
+			RTurret.localRotation = Quaternion.Lerp (oldRight.localRotation, Quaternion.Euler (0, Mathf.Atan2 (XCI.GetAxis (XboxAxis.LeftStickX), XCI.GetAxis (XboxAxis.LeftStickY)) * 180 / Mathf.PI, 0), .5f);
 		}
 
 		if (XCI.GetButton (XboxButton.RightStick)) {
